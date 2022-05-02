c7d39b1242583818aa4383b27cd21062ce7e7de4
Miguel Mendez <miguel@emerald-isle.eu>
2022-05-02 10:18:16 +0100

    Backport bugfix.

--- pkg/vec3/vec3.go
+++ pkg/vec3/vec3.go
@@ -1,7 +1,10 @@
 // Package vec3 provides utility functions to work with vectors.
 package vec3
 
-import "math"
+import (
+	"math"
+	"math/rand"
+)
 
 // Vec3Impl defines a vector with its position and colour.
 type Vec3Impl struct {
@@ -25,9 +28,10 @@ func (v *Vec3Impl) SquaredLength() float64 {
 
 // MakeUnitVector transform the vector into its unit representation.
 func (v *Vec3Impl) MakeUnitVector() {
-	v.X = v.X / v.Length()
-	v.Y = v.Y / v.Length()
-	v.Z = v.Z / v.Length()
+	l := v.Length()
+	v.X = v.X / l
+	v.Y = v.Y / l
+	v.Z = v.Z / l
 }
 
 // Add returns the sum of two or more vectors.
@@ -117,4 +121,46 @@ func Cross(v1 *Vec3Impl, v2 *Vec3Impl) *Vec3Impl {
 // UnitVector returns a unit vector representation of the supplied vector.
 func UnitVector(v *Vec3Impl) *Vec3Impl {
 	return ScalarDiv(v, v.Length())
+}
+
+// RandomCosineDirection returns a vector with a random cosine direction.
+func RandomCosineDirection() *Vec3Impl {
+	r1 := rand.Float64()
+	r2 := rand.Float64()
+	z := math.Sqrt(1 - r2)
+	phi := 2 * math.Pi * r1
+	x := math.Cos(phi) * 2 * math.Sqrt(r2)
+	y := math.Sin(phi) * 2 * math.Sqrt(r2)
+	return &Vec3Impl{X: x, Y: y, Z: z}
+}
+
+// RandomToSphere returns a new random sphere of the given radius at the given distance.
+func RandomToSphere(radius float64, distanceSquared float64) *Vec3Impl {
+	r1 := rand.Float64()
+	r2 := rand.Float64()
+	z := 1 + r2*(math.Sqrt(1-radius*radius/distanceSquared)-1)
+	phi := 2 * math.Pi * r1
+	x := math.Cos(phi) * math.Sqrt(1-z*z)
+	y := math.Sin(phi) * math.Sqrt(1-z*z)
+	return &Vec3Impl{X: x, Y: y, Z: z}
+}
+
+// DeNAN ensures that the vector elements are numbers.
+func DeNAN(v *Vec3Impl) *Vec3Impl {
+	x := v.X
+	y := v.Y
+	z := v.Z
+	if math.IsNaN(x) || math.IsInf(x, -1) || math.IsInf(x, 1) {
+		x = 0
+	}
+
+	if math.IsNaN(y) || math.IsInf(y, -1) || math.IsInf(y, 1) {
+		y = 0
+	}
+
+	if math.IsNaN(z) || math.IsInf(z, -1) || math.IsInf(z, 1) {
+		z = 0
+	}
+
+	return &Vec3Impl{X: x, Y: y, Z: z}
 }
